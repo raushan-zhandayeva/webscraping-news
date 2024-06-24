bf2649effd0d7335f75bc15c73bdf3da7dda89e6
Raushan Zhandayeva <109552707+raushan-zhandayeva@users.noreply.github.com>
2024-06-24 18:54:52 -0500

    Add files via upload
    This Python script processes HTML files from a specified directory, using BeautifulSoup to extract article headlines, publication dates, and main text content from Channel 1 news articles. It handles text cleaning, organizes the extracted data into a pandas DataFrame, and saves the results as an Excel file, automating the scraping process for multiple HTML files.

---
+++ channel1_scraping.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  2 21:09:05 2024
+
+@author: raushanzhandayeva
+"""
+
+# setting up 
+from bs4 import BeautifulSoup #for parsing html
+import os #for file manipulation
+import pandas as pd #for data manipulation
+
+#directory
+new_directory = "/Users/raushanzhandayeva/Desktop/Dissertation/Channel1-text/01. January 2022 - Channel 1" # change this to the directory where the HTML files are stored
+os.chdir(new_directory)
+
+# Initialize an empty list to store the data
+data = []
+
+# Loop through all the files in the directory
+for filename in os.listdir(new_directory):
+    if filename.endswith(".htm"):  # Check if the file has a .htm extension
+    
+        # Open the HTML file
+        with open(filename, "r", encoding="windows-1251") as file:
+            contents = file.read()
+
+        # Parsing the HTML content
+        soup = BeautifulSoup(contents, 'html.parser')
+
+        # Finding the title
+        headline = soup.find('title').text.strip()
+
+        # Finding the date from the <meta> tag
+        date_meta = soup.find('meta', attrs={'name': '_YR'})
+        if date_meta:
+            full_date = date_meta['content']
+            date_published = full_date.split(' ')[0]  # Extract the date portion
+        else:
+            date_published = ''
+
+        # Finding the text
+        text_element = soup.find('p')
+        if text_element:
+            text = text_element.get_text(separator=' ').strip()
+            # Remove the "Видеосюжет" parts using string manipulation
+            text = text.split("Видеосюжет")[0].strip()
+        else:
+            text = ''
+
+        # Remove the "Видеосюжет" parts using string manipulation
+        text = text.split("Видеосюжет")[0].strip()
+
+        # Append the extracted data to the list
+        data.append({'Headline': headline, 'date_published': date_published, 'text': text})
+
+# Create a DataFrame from the extracted data
+df = pd.DataFrame(data)
+
+# Save the DataFrame as a CSV file
+df.to_excel("01_scraped_ch1_rus_jan22.xlsx", index=False) # change the name of the output file as needed (month)
+
+
+
